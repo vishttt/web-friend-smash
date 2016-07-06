651f237bc4f13015ec37352ba6755a2c238cdc08
vishttt <vishttt@gmail.com>
2016-07-06 23:44:03 +0700

    Update config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
     - create a new App and retrieve the app ID and namespace from Facebook
     Developer site at https://developers.facebook.com/apps
 */
-  $serverUrl = "https://friendsmashsample.herokuapp.com";
-  $appNamespace = "friendsmashsample";
-  $appId = "480369938658210";
+  $serverUrl = "https://zezis.herokuapp.com";
+  $appNamespace = "zezis";
+  $appId = "887465754698483";
 ?>
